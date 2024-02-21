2404084ed41950a1d6ad3c24a61960038ab1ad83
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2024-02-21 15:29:14 -0400

    melhoramentos

--- alg/algoritmos/cap02/input-output.js
+++ alg/algoritmos/cap02/input-output.js
@@ -6,5 +6,5 @@ let name = scanf('%s');
 console.log('Please input your age');
 let age = scanf('%d');
 
-console.log('your name [%s] type: [%s]', name, typeof name);
-console.log('your age [%s] type: [%s]', age, typeof age);
+console.log('your name', name, 'is a', typeof name);
+console.log('your age', age, 'is a', typeof age);
