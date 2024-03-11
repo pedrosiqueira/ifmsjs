f36d8d7651bd88adb8fa7555a76a670a129522b7
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2024-03-11 18:43:24 -0400

    up

--- alg/algoritmos/cap06/if-aninhado-3.js
+++ alg/algoritmos/cap06/if-aninhado-3.js
@@ -1,27 +1,25 @@
 import scanf from "scanf";
-
-console.log("Boletim Digital");
-console.log("###############\n");
-
-console.log("Trabalho:");
-let t = scanf("%f");
-console.log("Seminário:");
-let s = scanf("%f");
-console.log("Prova:");
-let p = scanf("%f");
-
-let m = (t * 2 + s * 3 + p * 5) / 10;
-
-console.log("\nMédia:", m);
-
-if (m >= 8.5) {
-    console.log("Conceito A, aprovado :D");
-} else if (m >= 7) {
-    console.log("Conceito B, aprovado :)");
-} else if (m >= 5) {
-    console.log("Conceito C, recuperação :|");
-} else if (m >= 2.5) {
-    console.log("Conceito D, recuperação :(");
+ 
+console.log("Digite 3 números...");
+let x = scanf('%d');
+let y = scanf('%d');
+let z = scanf('%d');
+let maior;
+ 
+if (x > y) {
+    maior = x;
 } else {
-    console.log("Conceito E, reprovado :O");
+    maior = y;
+}
+ 
+if (z > maior) {
+    maior = z;
+}
+ 
+console.log(maior, "é o maior número");
+ 
+if (maior % 2 != 0) {
+    console.log("e também é ímpar");
+} else {
+    console.log("e também é par");
 }
--- alg/algoritmos/cap06/if.js
+++ alg/algoritmos/cap06/if.js
@@ -1,35 +1,38 @@
 import scanf from "scanf";
-
+ 
 console.log("Informe teu salario: ");
 let salario = scanf("%f")
-console.log("Informe tua nota: ");
-let nota = scanf("%f")
-console.log("Informe teu numero da sorte: ");
-let numero = scanf("%d")
-console.log("Informe tua idade: ");
-let idade = scanf("%d")
-
+ 
 console.log("\nVocê ganha", salario);
 if (salario > 5000) {
     console.log("Você está bem, ein!");
 }
-
+ 
+console.log("Informe tua nota: ");
+let nota = scanf("%f")
+ 
 console.log("\nVocê tirou nota", nota);
 if (nota < 7)
 {
     console.log("Você está de recuperação!");
 }
-
+ 
+console.log("Informe teu numero da sorte: ");
+let numero = scanf("%d")
+ 
 console.log("\nTeu número da sorte é", numero);
 if (numero % 2 == 0) console.log("Número par");
-
+ 
 if (numero % 5 == 0) 
     console.log("Número múltiplo de 5.");
-
+ 
+console.log("Informe tua idade: ");
+let idade = scanf("%d")
+ 
 console.log("\nTua idade é", idade);
 if (idade >= 18) {
     console.log("Você é maior de idade,");
     console.log("Já é responsável legalmente.");
 }
-
+ 
 console.log("\nFim do programa.");
