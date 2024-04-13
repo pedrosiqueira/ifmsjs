79ba5063bc9f74ed5994a0dc46a39215ed3a0d06
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2024-04-13 20:03:51 -0300

    modified:   "alg/11 Repeti\303\247\303\243o for.html"

--- alg/codigos/cap10/01-while.js
+++ alg/codigos/cap10/01-while.js
@@ -1,33 +1,13 @@
 import scanf from 'scanf';
 
-// Inicialize as variáveis para contar os aprovados e reprovados
-let aprovados = 0
-let reprovados = 0
+console.log("Informe o capital inicial:")
+let C = scanf("%f")
+console.log("Informe a taxa de juros anual (por exemplo, 0.05 para 5%):")
+let i = scanf("%f")
+console.log("Informe a quantidade de anos de investimento:")
+let t = scanf("%f")
 
-// Inicialize a variável para contar o número de estudantes
-let contador = 0
-
-// Use um loop while para ler os resultados de cada estudante
-while (contador < 10) {
-    console.log("Digite o nome do estudante:")
-    let nome = scanf("%s")
-    console.log("Digite o resultado do estudante ('a' para aprovado ou 'r' para reprovado):")
-    let resultado = scanf("%s")
-
-    // Verifique o resultado e atualize as contagens
-    if (resultado == "a") {
-        aprovados = aprovados + 1
-    } else {
-        reprovados = reprovados + 1
-    }
-    contador = contador + 1
-}
-
-// Escreva a quantidade de estudantes aprovados e reprovados
-console.log("Quantidade de estudantes aprovados:", aprovados)
-console.log("Quantidade de estudantes reprovados:", reprovados)
-
-// Verifique se mais de oito estudantes foram aprovados
-if (aprovados > 8) {
-    console.log("Bônus ao instrutor!")
+for (let ano = 1; ano <= t; ano++) {
+    let M = C * (1 + i) ** ano
+    console.log(`Ao final do ${ano}º ano, o montante será de ${M}`)
 }
