2aaf88ebdea702738a5bbfec2ff8aa9c9fa79294
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2024-04-15 13:38:23 -0400

    modified:   "alg/14 Fun\303\247\303\265es.html"

--- alg/codigos/cap13/01-funções.js
+++ alg/codigos/cap13/01-funções.js
@@ -1,15 +1,11 @@
-// Para invocar uma função que retorna um valor, usamos uma variável para armazenar o valor retornado pela função
-const n32 = Math.pow(2, 5)
-console.log(n32) // 32.0
+function soma(x, y) {
+    console.log('calculando soma...')
+    const z = x + y
+    return z
+}
 
-// Podemos imprimir diretamente o valor retornado por uma função
-console.log(Math.pow(5, 3)) // 125.0
+const a = soma(10, 20)
+console.log(a)
 
-// Podemos passar uma expressão como argumento, desde que seu valor seja equivalente ao argumento esperado pela função
-console.log(Math.pow(2 * 3 - 1, -5 + 16 / 2)) // 125.0
-
-// Podemos até usar o retorno de uma função como argumento para outra função
-const cateto1 = 6
-const cateto2 = 8
-const hipotenusa = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2))
-console.log(hipotenusa) // 10.0
+const b = 30
+console.log(soma(a, b))
---
+++ alg/codigos/cap13/modulo_principal.js
@@ -0,0 +1,4 @@
+import { nome, saudacoes } from './modulo_importado.js'
+
+saudacoes(nome)
+saudacoes('Henrique')
