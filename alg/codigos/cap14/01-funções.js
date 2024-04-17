6baee8aa80bf124c4f3c660c2985010302e7dbfb
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2024-04-17 12:00:46 -0300

    up

--- alg/codigos/cap14/01-funções.js
+++ alg/codigos/cap14/01-funções.js
@@ -6,16 +6,21 @@ function multiplica_por_dois(numero) {
     return numero * 2
 }
 
-const s = '2.4'
-const f = parseFloat(s)
-const i = parseInt(s)
-
-console.log(soma_com_dois('dois'))
+const s = 'dois'
 console.log(soma_com_dois(s))
-console.log(soma_com_dois(f))
-console.log(soma_com_dois(i))
-console.log()
-console.log(multiplica_por_dois('dois'))
 console.log(multiplica_por_dois(s))
+console.log()
+
+const n = '2.4'
+console.log(soma_com_dois(n))
+console.log(multiplica_por_dois(n))
+console.log()
+
+const f = parseFloat(n)
+console.log(soma_com_dois(f))
 console.log(multiplica_por_dois(f))
+console.log()
+
+const i = parseInt(n)
+console.log(soma_com_dois(i))
 console.log(multiplica_por_dois(i))
