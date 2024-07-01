adbfa37c626ce19b3b2a79db6eeae98faa96f30b
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2024-07-01 12:44:22 -0400

    up

--- alg/codigos/cap14/01-funções.js
+++ alg/codigos/cap14/01-funções.js
@@ -1,11 +1,26 @@
-function soma(x, y) {
-    console.log('calculando soma...')
-    const z = x + y
-    return z
+function soma_com_dois(numero) {
+    return numero + 2
 }
 
-const a = soma(10, 20)
-console.log(a)
+function multiplica_por_dois(numero) {
+    return numero * 2
+}
 
-const b = 30
-console.log(soma(a, b))
+const s = 'dois'
+console.log(soma_com_dois(s))
+console.log(multiplica_por_dois(s))
+console.log()
+
+const n = '2.4'
+console.log(soma_com_dois(n))
+console.log(multiplica_por_dois(n))
+console.log()
+
+const f = parseFloat(n)
+console.log(soma_com_dois(f))
+console.log(multiplica_por_dois(f))
+console.log()
+
+const i = parseInt(n)
+console.log(soma_com_dois(i))
+console.log(multiplica_por_dois(i))
