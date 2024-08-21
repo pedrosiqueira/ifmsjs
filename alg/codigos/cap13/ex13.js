aa99f82c6281c4ef899de1780186cc27d01f042b
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2024-08-21 15:26:47 -0400

    up

--- alg/codigos/cap13/ex13.js
+++ alg/codigos/cap13/ex13.js
@@ -11,10 +11,18 @@ for (let i = 0; i < n; i++) {
     v.push(scanf("%d"))
 }
 
+let ha_repetidos = false
+ 
 for (let i = 0; i < n; i++) {
     for (let j = i + 1; j < n; j++) {
         if (v[i] == v[j]) {
             console.log("Sim!", v[i], "se repete!")
+            ha_repetidos = true
+            i = j = n
         }
     }
+}
+ 
+if (!ha_repetidos) {
+    console.log("Não há repetidos!")
 }
