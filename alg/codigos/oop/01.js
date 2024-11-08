ccdd0318127f490e8ed3e2cbb7790303f499672f
opedrosiqueira <opedrosiqueira@gmail.com>
2024-11-08 09:49:17 -0400

    introdução a objetos

--- alg/codigos/oop/01.js
+++ alg/codigos/oop/01.js
@@ -32,3 +32,45 @@ person.farewell = function () {
 
 console.log(person["eyes"]); // hazel
 person.farewell(); // Bye everybody!
+
+console.log(person.height); // undefined
+const myDataName = "height";
+const myDataValue = "1.75m";
+person[myDataName] = myDataValue;
+console.log(person.height); // 1.75m
+
+const first = "Alice";
+const last = "Johnson";
+const age = 40;
+const otherperson = { name: { first, last }, age };
+
+console.log(otherperson.name, otherperson.age) // { first: 'Alice', last: 'Johnson' } 40
+
+
+const introduceSelf = function () {
+    console.log(`Hi! I'm ${this.name}.`);
+};
+
+const person1 = { name: 'Chris', introduceSelf };
+const person2 = { name: 'Deepti', introduceSelf };
+
+person1.introduceSelf();
+person2.introduceSelf();
+
+function Person(name) {
+    this.name = name;
+    this.introduceSelf = function () {
+        console.log(`Hi! I'm ${this.name}.`);
+    };
+}
+
+const person3 = new Person("Salva");
+person3.introduceSelf(); // "Hi! I'm Salva."
+
+const person4 = new Person("Frankie");
+person4.introduceSelf(); // "Hi! I'm Frankie."
+
+const atext = new String('foo')
+const alist = new Array(atext)
+alist.push('bar')
+console.log(alist[0].toUpperCase(), alist[1]) // FOO bar
