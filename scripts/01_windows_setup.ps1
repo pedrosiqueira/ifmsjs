84e89c528316e260e673661b1779e88139d47985
opedrosiqueira <opedrosiqueira@gmail.com>
2025-03-12 18:47:12 -0400

    pc

--- scripts/01_windows_setup.ps1
+++ scripts/01_windows_setup.ps1
@@ -42,4 +42,7 @@ Add-PathIfNotExists -newPath "$env:SYSTEMDRIVE\npm\pnpm\"
 
 # recarrega as variáveis de ambiente modificadas
 # refreshenv
+
+Read-Host "Instalação finalizada. Tecle algo para sair..."
+
 exit # sair do terminal é mais garantido para recarregar as variáveis de ambiente
